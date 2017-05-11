7f0c996754aab7c1494d87a1a7a005e35dd5433a
Ollie <reelfeet@hotmail.com>
2017-05-11 20:14:21 +0100

    Add result class

--- bets/models.py
+++ bets/models.py
@@ -3,13 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from django import forms
+
+from wagtail.wagtailsnippets.models import register_snippet
+
 # Create your models here.
+@register_snippet
 class Season(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
+@register_snippet
 class Gameweek(models.Model):
     season = models.ForeignKey(Season, on_delete=models.CASCADE)
     number = models.IntegerField(default=0)
@@ -18,6 +24,7 @@ class Gameweek(models.Model):
     def __str__(self):
         return self.number
 
+@register_snippet
 class Game(models.Model):
     gameweek = models.ForeignKey(Gameweek, on_delete=models.CASCADE)
     hometeam = models.CharField(max_length=255)
@@ -31,3 +38,9 @@ class Game(models.Model):
 
     def __str__(self):
         return self.hometeam + " vs " + awayteam
+
+@register_snippet
+class Result(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    RESULTS = (('H', 'Home'), ('D', 'Draw'), ('A', 'Away'))
+    result = models.CharField(max_length=1, choices=RESULTS)
