e7c87d4395c19a2a9c8cec266a06516d06447441
Ollie - Python Anywhere <reelfeet@hotmail.com>
2018-05-21 15:23:24 +0000

    Commit prod versions of migrations

--- fglsite/settings/production.py
+++ fglsite/settings/production.py
@@ -9,8 +9,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 STATIC_ROOT = '/home/olliefgl/fglsite/static/'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+#EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+#EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
 
