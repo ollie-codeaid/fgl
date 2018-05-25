1620587eca719ea69ea7cc90c7cac62a5ec1a22d
Ollie <reelfeet@hotmail.com>
2018-05-25 08:39:59 +0100

    Add update gameweek test

--- bets/tests/test_views.py
+++ bets/tests/test_views.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 import datetime
 
-from bets.models import Season
+from bets.models import Season, Gameweek, Game
 from django.contrib.auth.models import Group
 from django.urls import reverse
 
@@ -15,22 +15,23 @@ def _create_test_season():
 
 class ViewsTest(TestCase):
 
-    management_data = {
-        'form-TOTAL_FORMS': '1',
-        'form-INITIAL_FORMS': '0',
-        'form-MAX_NUM_FORMS': '',
-    }
-
-    def _create_game_data(self):
+    def _create_management_data(self, form_count):
         return {
-            'form-0-hometeam':'Manchester United',
-            'form-0-awayteam':'Chelsea',
-            'form-0-homenumerator':1,
-            'form-0-homedenominator':2,
-            'form-0-drawnumerator':3,
-            'form-0-drawdenominator':4,
-            'form-0-awaynumerator':5,
-            'form-0-awaydenominator':6,
+            'form-TOTAL_FORMS': form_count,
+            'form-INITIAL_FORMS': '0',
+            'form-MAX_NUM_FORMS': '',
+        }
+
+    def _create_game_data(self, game_num, home, away):
+        return {
+            'form-{0}-hometeam'.format(game_num):home,
+            'form-{0}-awayteam'.format(game_num):away,
+            'form-{0}-homenumerator'.format(game_num):1,
+            'form-{0}-homedenominator'.format(game_num):2,
+            'form-{0}-drawnumerator'.format(game_num):3,
+            'form-{0}-drawdenominator'.format(game_num):4,
+            'form-{0}-awaynumerator'.format(game_num):5,
+            'form-{0}-awaydenominator'.format(game_num):6,
         }
 
     def setUp(self):
@@ -42,16 +43,66 @@ class ViewsTest(TestCase):
         response = self.client.get(url)
         self.assertIn('test', response.content )
 
-    def test_create_gameweek(self):
-        season = _create_test_season()
-        data = {
+    def _create_basic_form_data(self):
+        gameweek_data = {
                 'deadline_date':datetime.date(2017, 12, 1),
                 'deadline_time':datetime.time(12, 00),
                 'spiel':'empty',
                 }
-        data.update(self.management_data)
-        data.update(self._create_game_data())
-        url = reverse('create-gameweek', args=(season.id,))
-        response = self.client.post(url, data=data)
-        self.assertEquals(200, response.status_code)
+        management_data = self._create_management_data(1)
+        game_data = self._create_game_data(0, 'Manchester Utd', 'Chelsea')
+
+        form_data = {}
+        form_data.update( gameweek_data )
+        form_data.update( management_data )
+        form_data.update( game_data)
+
+        return form_data
+
+    def _assert_base_gameweek_response_correct(self, response, data, season):
         self.assertEquals(1, len(season.gameweek_set.all()))
+
+        gameweek = season.gameweek_set.all()[0]
+        self.assertEquals(data['deadline_date'], gameweek.deadline_date)
+        self.assertEquals(data['deadline_time'], gameweek.deadline_time)
+        self.assertEquals(data['spiel'], gameweek.spiel)
+        self.assertEquals(1, len(gameweek.game_set.all()))
+        
+        game = gameweek.game_set.all()[0]
+        game_data = self._create_game_data(0, 'Manchester Utd', 'Chelsea')
+        for key, value in game_data.iteritems():
+            self.assertEquals(value, getattr(game, key.replace('form-0-', '')))
+
+    def test_create_gameweek(self):
+        season = _create_test_season()
+
+        form_data = self._create_basic_form_data()
+
+        url = reverse('create-gameweek', args=(season.id,))
+        response = self.client.post(url, data=form_data)
+
+        self._assert_base_gameweek_response_correct(response, form_data, season)
+
+    def test_update_gameweek(self):
+        season = _create_test_season()
+        gameweek = Gameweek(season=season,
+                deadline_date=datetime.date(2017, 11, 1),
+                deadline_time=datetime.time(13, 00),
+                spiel = 'not empty')
+        gameweek.save()
+        game = Game(gameweek=gameweek,
+                hometeam='Watford',
+                awayteam='Reading',
+                homenumerator=6, homedenominator=5,
+                drawnumerator=4, drawdenominator=3,
+                awaynumerator=2, awaydenominator=1)
+        game.save()
+
+        form_data = self._create_basic_form_data()
+
+        url = reverse('update-gameweek', args=(gameweek.id,))
+        response = self.client.post(url, data = form_data)
+
+        self._assert_base_gameweek_response_correct(response, form_data, season)
+
+
