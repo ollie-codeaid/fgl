28c08c084332232845e22fe999205a0b4e87cf8a
Ollie <reelfeet@hotmail.com>
2018-05-26 10:26:54 +0100

    Fix URL for create season

--- bets/urls.py
+++ bets/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^bets/create_season/$', views.create_season, name='create-season'),
+    url(r'^create_season/$', views.create_season, name='create-season'),
     url(r'^(?P<season_id>[0-9]+)/$', views.season, name='season'),
     url(r'^(?P<season_id>[0-9]+)/create_gameweek/$', views.create_gameweek, name='create-gameweek'),
     url(r'^gameweek/(?P<gameweek_id>[0-9]+)/$', views.gameweek, name='gameweek'),
