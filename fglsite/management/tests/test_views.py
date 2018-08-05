62731d854e9a259854b5ff9d2e160493c2fa0303
Ollie <reelfeet@hotmail.com>
2018-08-05 15:50:58 +0100

    Move tests

---
+++ fglsite/management/tests/test_views.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+
+from fglsite.bets.models import Season
+from fglsite.management.models import JoinRequest
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class ViewsTest(TestCase):
+
+    def setUp(self):
+        commissioner = User.objects.create_user(
+                username='comm',
+                password='comm')
+        self.season = Season.objects.create(
+                name='test',
+                commissioner=commissioner,
+                weekly_allowance=100.0)
+
+    def test_join_season(self):
+        player_one = User.objects.create_user(
+                username='player_one', password='pass')
+
+        url = reverse('join-season', args=(self.season.id,))
+        self.client.login(username='player_one', password='pass')
+        self.client.post(url)
+        self.client.logout()
+
+        self.assertEquals(1, len(self.season.joinrequest_set.all()))
+        self.assertEquals(
+                player_one,
+                self.season.joinrequest_set.first().player)
+
+    def test_join_season_public(self):
+        player_one = User.objects.create_user(
+                username='player_one', password='pass')
+        self.season.public = True
+        self.season.save()
+
+        url = reverse('join-season', args=(self.season.id,))
+        self.client.login(username='player_one', password='pass')
+        self.client.post(url)
+        self.client.logout()
+
+        self.assertIn(player_one, self.season.players.all())
+
+    def test_accept_request(self):
+        player_one = User.objects.create_user(
+                username='player_one', password='pass')
+        request = JoinRequest(season=self.season, player=player_one)
+        request.save()
+
+        url = reverse('accept-request', args=(request.id,))
+        self.client.login(username='comm', password='comm')
+        self.client.post(url)
+        self.client.logout()
+
+        self.assertEquals(0, len(self.season.joinrequest_set.all()))
+        self.assertIn(player_one, self.season.players.all())
+
+    def test_reject_request(self):
+        player_one = User.objects.create_user(
+                username='player_one', password='pass')
+        request = JoinRequest(season=self.season, player=player_one)
+        request.save()
+
+        url = reverse('reject-request', args=(request.id,))
+        self.client.login(username='comm', password='comm')
+        self.client.post(url)
+        self.client.logout()
+
+        self.assertEquals(0, len(self.season.joinrequest_set.all()))
+        self.assertNotIn(player_one, self.season.players.all())
