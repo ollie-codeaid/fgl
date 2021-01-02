815a4fdae82f5abb070992e3a3578e65a5683392
Ollie <reelfeet@hotmail.com>
2021-01-02 10:25:17 +0000

    Hide long term management links from gameweek detail view

--- fglsite/gambling/templatetags/gambling_extras.py
+++ fglsite/gambling/templatetags/gambling_extras.py
@@ -4,9 +4,9 @@ register = template.Library()
 
 
 @register.inclusion_tag("gambling/long_term_odds.html")
-def long_term_odds(container, gameweek, request):
+def long_term_odds(container, gameweek, show_management_links):
     return {
         "container": container,
         "gameweek": gameweek,
-        "request": request,
+        "show_management_links": show_management_links,
     }
